f287f0056abbbdb88fd08ebd1e6e867fd1034d89
Jonathan Freihofer <patientfrei@gmail.com>
2021-05-01 10:54:03 -0400

    styled components/respective folders

---
+++ src/Components/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StyledButton from "./styles"
+
+const Button = () => {
+    return (
+        <StyledButton>
+            <button></button>
+        </StyledButton>
+    )
+}
+
+export default Button;
