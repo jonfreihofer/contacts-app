e0abcba28fb0820ddd0e08cb0bf22472e9aa6c3f
Jonathan Freihofer <patientfrei@gmail.com>
2021-05-03 19:28:21 -0400

    changed

--- src/Components/Contact/styles.js
+++ src/Components/Contact/styles.js
@@ -1,17 +1,16 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const StyledContact = styled.div`
-    color: white;
-    background-color: #23a6d5  ;
-    max-width: 40%;
-    margin: .35rem;
-    padding: .75rem;
-    border-radius: 3%;
-    opacity: .8;
-    -webkit-box-shadow: 3px 3px 5px 6px #ccc;  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
-    -moz-box-shadow:    3px 3px 5px 6px #ccc;  /* Firefox 3.5 - 3.6 */
-    box-shadow:         3px 3px 5px 6px #ccc; 
-        
-`
+  color: white;
+  background-color: #23a6d5;
+  max-width: 40%;
+  margin: 0.35rem;
+  padding: 0.75rem;
+  border-radius: 3%;
+  opacity: 0.8;
+  -webkit-box-shadow: 3px 3px 5px 6px #ccc;
+  -moz-box-shadow: 3px 3px 5px 6px #ccc;
+  box-shadow: 3px 3px 5px 6px #ccc;
+`;
 
-export default StyledContact
+export default StyledContact;
