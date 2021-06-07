17144eada797ed997afd6140ed6c231eb834d2fe
Jonathan Freihofer <patientfrei@gmail.com>
2021-06-07 07:53:42 -0400

    commit

--- src/Components/PopUp/styles.js
+++ src/Components/PopUp/styles.js
@@ -3,14 +3,15 @@ import styled from "styled-components";
 const StyledPopUp = styled.div`
   margin: 0 auto;
   width: 500px;
-  height: 200px;
+  height: 250px;
   background: #23d5ab;
   -webkit-box-shadow: 3px 3px 5px 6px #ccc;
   -moz-box-shadow: 3px 3px 5px 6px #ccc;
   box-shadow: 3px 3px 5px 6px #ccc;
   display: ${({ showPopUp }) => (showPopUp ? "flex" : "none")};
   flex-direction: row;
-  justify-conent: space-between;
+  flex-wrap: wrap;
+  justify-content: center;
 
   h2 {
     padding: 4rem;
@@ -18,6 +19,15 @@ const StyledPopUp = styled.div`
   }
   button {
     height: 40px;
+    background-color: purple;
+    font-weight: bolder;
+    text-transform: uppercase;
+    color: white;
+    border: none;
+    margin: 3px;
+    -webkit-box-shadow: 3px 3px 5px 6px #ccc;
+    -moz-box-shadow: 3px 3px 5px 6px #ccc;
+    box-shadow: 3px 3px 5px 6px #ccc;
   }
 `;
 export default StyledPopUp;
