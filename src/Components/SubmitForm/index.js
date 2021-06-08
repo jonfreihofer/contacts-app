3033b47d3d3d5d307beac8f5debbd2edc6c5c49b
Jonathan Freihofer <patientfrei@gmail.com>
2021-06-08 18:54:05 -0400

    context
    g

--- src/Components/SubmitForm/index.js
+++ src/Components/SubmitForm/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Context } from "../Context";
+import { Context } from "../Context.js";
 import Button from "../Button";
 import { StyledForm, StyledInput } from "./styles";
 
