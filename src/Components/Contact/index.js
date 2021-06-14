54d2847a4ab58a7f02ef9b4a1cba75e284c09d40
Jonathan Freihofer <patientfrei@gmail.com>
2021-06-14 08:16:40 -0400

    editMode

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -15,18 +15,30 @@ function Contact({
   const [isFavorited, setIsFavorited] = useState(false);
   const [showPopUp, setShowPopUp] = useState(false);
   const [edit, setEdit] = useState(false);
-  const { inputRef, setContactsData, handleChange } = useContext(Context);
+  const { inputRef, setContactsData, setInputData, handleChange } =
+    useContext(Context);
 
   const upDateValue = (id) => {
+    const split = inputRef.current.value.split(" ");
+    setContactsData((prevContact) =>
+      prevContact.sort((a, b) => a.lastName.localeCompare(b.lastName))
+    );
     setContactsData((prevContact) =>
       prevContact.filter((contact) =>
         contact.id === id
-          ? (contact.editName = inputRef.current.value)
+          ? ((contact.firstName = split[0]), (contact.lastName = split[1]))
           : contact
       )
     );
-    // editName = inputRef.current.value;
-    // setContactsData((prevContacts) => [...prevContacts, editName]);
+    console.log(lastName);
+    setInputData((prevInputData) => ({
+      id: prevInputData.id,
+      firstName: "",
+      lastName: "",
+      editName: "",
+    }));
+
+    // todo: change editName to replace both firstName and lastName values, for purpose of data continuity
     setEdit(false);
   };
 
@@ -61,6 +73,7 @@ function Contact({
         showPopUp={showPopUp}
         setShowPopUp={setShowPopUp}
         firstName={firstName}
+        lastName={lastName}
         removeContact={removeContact}
         id={id}
       />
--- src/Components/PopUp/index.js
+++ src/Components/PopUp/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import StyledPopUp from "./styles";
 
-function PopUp({ showPopUp, setShowPopUp, firstName, removeContact, id }) {
+function PopUp({
+  showPopUp,
+  setShowPopUp,
+  firstName,
+  lastName,
+  removeContact,
+  id,
+}) {
+  const renderName = () => {
+    return <div>{firstName ? `${firstName}` : `${lastName}`}</div>;
+  };
   return (
     <>
       <StyledPopUp showPopUp={showPopUp}>
-        <h2> Remove {firstName} from Contacts?</h2>
+        <h2> Remove {renderName()}?</h2>
         <button onClick={() => setShowPopUp(!showPopUp)}>No</button>
         <button onClick={() => removeContact(id)}>Yes</button>
       </StyledPopUp>
